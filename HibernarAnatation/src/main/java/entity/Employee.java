d3b1c1f73a680f4cbf541f7d52343dc081d8ccf9
ChabanovSN <chabanovsn@mail.ru>
2018-04-03 20:22:02 +0300

    Initial commit

---
+++ HibernarAnatation/src/main/java/entity/Employee.java
@@ -0,0 +1,116 @@
+package entity;
+
+import javax.persistence.*;
+
+import java.sql.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "BIRTHDAY")
+    private Date birthDay;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+    @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "EMPL_PROJ",
+            joinColumns = @JoinColumn(name = "EMPLOYEE_ID"),
+            inverseJoinColumns = @JoinColumn(name = "PROJECT_ID")
+    )
+    private Set<Project> projects;
+    public Employee(){}
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Set<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<Project> projects) {
+        this.projects = projects;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDay=" + birthDay +
+                ", address=" + address +
+                ", projects=" + projects +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != null ? !id.equals(employee.id) : employee.id != null) return false;
+        if (firstName != null ? !firstName.equals(employee.firstName) : employee.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(employee.lastName) : employee.lastName != null) return false;
+        if (birthDay != null ? !birthDay.equals(employee.birthDay) : employee.birthDay != null) return false;
+        if (address != null ? !address.equals(employee.address) : employee.address != null) return false;
+        return projects != null ? projects.equals(employee.projects) : employee.projects == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (birthDay != null ? birthDay.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        result = 31 * result + (projects != null ? projects.hashCode() : 0);
+        return result;
+    }
+}
---
+++ HibernarAnatation/src/main/java/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+@Query(value = "SELECT * FROM EMPLOYEE WHERE FIRST_NAME=?1 and LAST_NAME=?2" ,nativeQuery = true)
+Employee findByFirstNameAndLastName(String firstName, String lastName);
+
+Employee findByLastName(String lastName);
+}
